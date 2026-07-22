782d552aef08af1ad39f604bcfc5c19a5d451a99
greatbrick <speharley@gmail.com>
2026-07-22 17:34:10 -0400

    Added New Floor

--- gmtk-jam-2026/Scripts/main_menu.gd
+++ gmtk-jam-2026/Scripts/main_menu.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
+@export var levelPath: String = "res://Scenes/open_level.tscn"
+
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/TestLevel.tscn")
+	get_tree().change_scene_to_file(levelPath)
--- gmtk-jam-2026/Scripts/player.gd
+++ gmtk-jam-2026/Scripts/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
-@export var speed: float = 30
+@export var speed: float = 45
 var distortion: Vector2 = Vector2(2, 1)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if(direction):
-		velocity = direction * distortion * speed
+		velocity = direction.normalized() * distortion * speed
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
