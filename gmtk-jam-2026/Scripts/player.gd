242ae7ec25491bf1e479a39141353df55262004f
greatbrick <speharley@gmail.com>
2026-07-22 15:04:45 -0400

    Added Test Level

---
+++ gmtk-jam-2026/Scripts/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+@export var speed: float = 30
+
+func _physics_process(delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if(direction):
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()
