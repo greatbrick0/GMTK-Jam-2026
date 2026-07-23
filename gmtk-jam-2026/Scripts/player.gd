b458328e5960271a4dac4ee9fdffdffe5181264f
greatbrick <speharley@gmail.com>
2026-07-23 16:08:37 -0400

    Added Sprint

--- gmtk-jam-2026/Scripts/player.gd
+++ gmtk-jam-2026/Scripts/player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed: float = 45
+var speedMult: float = 1.0
 @export var drainSpeed: float = 1.0
 @export var canMove: bool = false
 var distortion: Vector2 = Vector2(2, 1)
@@ -13,13 +14,17 @@ func _ready():
 	countDownRef.outOfSteps.connect(SetCanMove.bind(false))
 
 func _physics_process(delta):
+	speedMult = 2.5 if(Input.is_action_pressed("sprint")) else 1.0
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if(direction and canMove):
-		velocity = direction.normalized() * distortion * speed
-		countDownRef.DrainSteps(drainSpeed * delta)
+		velocity = direction.normalized() * distortion * speed * speedMult
+		countDownRef.DrainSteps(speedMult * drainSpeed * delta)
 	else:
 		velocity = Vector2.ZERO
 	move_and_slide()
 
 func SetCanMove(newCanMove: bool) -> void:
 	canMove = newCanMove
+
+func _on_interact_area_area_entered(area):
+	pass # Replace with function body.
