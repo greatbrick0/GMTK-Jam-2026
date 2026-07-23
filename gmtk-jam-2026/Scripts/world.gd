1f34bb4ec91cf02fe038b0bf0aa4af31189414b6
greatbrick <speharley@gmail.com>
2026-07-23 14:39:41 -0400

    Added Level Loading

--- gmtk-jam-2026/Scripts/world.gd
+++ gmtk-jam-2026/Scripts/world.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+@export var playerObj: PackedScene = preload("res://scenes/player.tscn")
 var playerRef: Player
+var levelRef: Level
 
-func SpawnLevel() -> void:
-	pass
+func _ready() -> void:
+	SpawnLevelFromPath("res://scenes/open_level.tscn")
+
+func SpawnLevelFromPacked(newLevel: PackedScene) -> void:
+	InitializeLevel(newLevel.instantiate())
+
+func SpawnLevelFromPath(newLevel: String) -> void:
+	InitializeLevel(load(newLevel).instantiate())
+
+func InitializeLevel(newLevel: Level) -> void:
+	$Level.add_child(newLevel)
+	levelRef = newLevel
+	SpawnPlayer()
 
 func SpawnPlayer() -> void:
-	pass
+	if(not playerRef):
+		playerRef = playerObj.instantiate()
+	levelRef.playerLayer.add_child(playerRef)
