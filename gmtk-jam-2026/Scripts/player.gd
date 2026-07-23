c42ace996f18db4fde70b99f449a69ca663b1076
greatbrick <speharley@gmail.com>
2026-07-23 01:48:42 -0400

    Added Hud

--- gmtk-jam-2026/Scripts/player.gd
+++ gmtk-jam-2026/Scripts/player.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
 @export var speed: float = 45
+@export var canMove: bool = false
 var distortion: Vector2 = Vector2(2, 1)
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	if(direction):
+	if(direction and canMove):
 		velocity = direction.normalized() * distortion * speed
 	else:
 		velocity = Vector2.ZERO
