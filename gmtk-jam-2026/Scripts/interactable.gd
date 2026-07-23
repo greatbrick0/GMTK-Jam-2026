ac913e2dfb7c98a9a5a6e1c28aa2125908f43348
greatbrick <speharley@gmail.com>
2026-07-23 17:14:34 -0400

    Added Interactable Class

---
+++ gmtk-jam-2026/Scripts/interactable.gd
@@ -0,0 +1,6 @@
+extends Area2D
+class_name Interactable
+
+func Interact() -> bool:
+	print("interacted")
+	return true
--- gmtk-jam-2026/Scripts/player.gd
+++ gmtk-jam-2026/Scripts/player.gd
@@ -7,11 +7,18 @@ var speedMult: float = 1.0
 @export var canMove: bool = false
 var distortion: Vector2 = Vector2(2, 1)
 
+var interactList: Array[Interactable]
+
 var countDownRef: CountDown
 
 func _ready():
 	countDownRef = get_tree().get_first_node_in_group("Countdown")
 	countDownRef.outOfSteps.connect(SetCanMove.bind(false))
+
+func _process(_delta):
+	if(Input.is_action_just_pressed("interact")):
+		if(len(interactList) > 0):
+			GetClosestInteractable().Interact()
 
 func _physics_process(delta):
 	speedMult = 2.5 if(Input.is_action_pressed("sprint")) else 1.0
@@ -27,4 +34,18 @@ func SetCanMove(newCanMove: bool) -> void:
 	canMove = newCanMove
 
 func _on_interact_area_area_entered(area):
-	pass # Replace with function body.
+	if(area is Interactable):
+		interactList.append(area)
+
+func _on_interact_area_area_exited(area):
+	if(area is Interactable):
+		interactList.erase(area)
+
+func GetClosestInteractable() -> Interactable:
+	var closestDistance: float = global_position.distance_squared_to(interactList[0].global_position)
+	var closestInteractable: Interactable = interactList[0]
+	for ii in interactList:
+		if(global_position.distance_squared_to(ii.global_position) < closestDistance):
+			closestDistance = global_position.distance_squared_to(ii.global_position)
+			closestInteractable = ii
+	return closestInteractable
