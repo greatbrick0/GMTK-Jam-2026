08b73d23c9e99d0a5150c69aac4c7d0ef2575ea2
Alexander <xrdharley@gmail.com>
2026-07-26 10:09:41 -0400

    Animated train on main menu

--- gmtk-jam-2026/Scripts/main_menu.gd
+++ gmtk-jam-2026/Scripts/main_menu.gd
@@ -5,3 +5,7 @@ extends Node2D
 func _on_start_button_pressed():
 	MusicManager.ChangeTrack(1)
 	get_tree().change_scene_to_file(levelPath)
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
