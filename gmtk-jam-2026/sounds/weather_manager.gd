927bede7b915f7386eebef24f69d06433aa5d87b
greatbrick <speharley@gmail.com>
2026-09-02 17:36:49 -0400

    Added Rain Fading

--- gmtk-jam-2026/sounds/weather_manager.gd
+++ gmtk-jam-2026/sounds/weather_manager.gd
@@ -4,9 +4,14 @@ var atTop: bool = true
 var inResetHud: bool = true
 
 var isRaining: bool = false
-var timeSinceWeatherChange: float = 0.0
+var timeSinceWeatherChange: float = 10.0
+
+@export var fadeSpeedMult: float = 3.0
+@export var rainOnVolume: float = 0.3
+@export var rainOffVolume: float = 0.0
 
 func _ready() -> void:
+	$AudioStreamPlayer.volume_linear = 0.0
 	$AudioStreamPlayer.playing = true
 
 func _process(delta):
@@ -22,9 +27,9 @@ func _process(delta):
 			timeSinceWeatherChange = 0.0
 	
 	if(isRaining):
-		$AudioStreamPlayer.volume_db = -6.0
+		$AudioStreamPlayer.volume_linear = lerpf(rainOffVolume, rainOnVolume, min(1.0, timeSinceWeatherChange * fadeSpeedMult))
 	else:
-		$AudioStreamPlayer.volume_db = -40.0
+		$AudioStreamPlayer.volume_linear = lerpf(rainOnVolume, rainOffVolume, min(1.0, timeSinceWeatherChange * fadeSpeedMult))
 
 func ChangeLevel(newLevel: int) -> void:
 	print(newLevel)
